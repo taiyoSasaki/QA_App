ae9776575bdd93f53261523a6bd6074576055655
taiyo.Sasaki <taiyo.sasaki@ods.co.jp>
2023-02-27 19:18:54 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/taiyo/sasaki/qa_app/Const.kt
@@ -0,0 +1,13 @@
+package jp.techacademy.taiyo.sasaki.qa_app
+
+//Firebaseにユーザの表示名を保存するパス
+const val UsersPATH = "users"
+
+//Firebaseに質問を保存するパス
+const val ContentsPATH = "contents"
+
+//Firebaseに回答を保存するパス
+const val AnswersPATH = "answers"
+
+//Preferenceに表示名を保存するパス
+const val NameKEY = "name"
